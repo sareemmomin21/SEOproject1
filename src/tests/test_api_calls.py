f92dcbba03c50d141a2611664c3a0ef2ad7a221f
timiray99 <ibitokun29@gmail.com>
2025-07-01 17:55:40 -0400

    test api

--- src/tests/test_api_calls.py
+++ src/tests/test_api_calls.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'api_handlers')))
 
 from food_facts_api import get_food_fact_from_barcode
-
 
 if __name__ == "__main__":
     test_barcode = "737628064502" 
